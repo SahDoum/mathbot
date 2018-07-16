740bca73f2c1394900b4b34bd9f7d267c99fcb90
gurland <stas.gurland@gmail.com>
2018-07-17 02:44:18 +0300

    Add more imports and add them into __all__ list

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,9 +1,27 @@
+from .admin import cmd_set_admin, cmd_set_moder
+from .book import cmd_add_book, inline_search_book
+from .catalog import cmd_lib, cmd_add_catalog
 from .courses import cmd_add_course
-from .default import cmd_welcome, cmd_lib, cmd_links
-from .subscriptions import cmd_set, subscription_menu_callback, subscription_menu_close_callback, \
-    subscription_menu_set_pressed_callback, subscription_menu_unset_pressed_callback, set_subscription_callback, \
-    unset_subscription_callback
+from .default import cmd_welcome, cmd_links
 
-__all__ = ['cmd_add_course', 'cmd_welcome', 'cmd_links', 'cmd_set', 'cmd_lib', 'subscription_menu_callback',
-           'subscription_menu_unset_pressed_callback', 'subscription_menu_set_pressed_callback',
-           'subscription_menu_close_callback', 'set_subscription_callback', 'unset_subscription_callback']
+from .subscriptions import cmd_set, cb_subscription_menu, cb_subscription_menu_close, \
+    cb_subscription_menu_set_pressed, cb_subscription_menu_unset_pressed, cb_set_subscription, \
+    cb_unset_subscription
+
+__all__ = [
+           # Simple administartion commands
+           'cmd_set_admin', 'cmd_set_moder',
+           # Cmds for adding books and inline search handler
+           'cmd_add_book', 'inline_search_book',
+           # Cmds for adding catalogs and launching catalog menu
+           'cmd_lib', 'cmd_add_catalog',
+           # Course adding cmd
+           'cmd_add_course',
+           # Simple "static" cmds
+           'cmd_welcome', 'cmd_links',
+           # Cmd to launch subscriptions menu
+           'cmd_set',
+           # Callback handlers for subscriptions menu
+           'cb_subscription_menu', 'cb_subscription_menu_unset_pressed', 'cb_subscription_menu_set_pressed',
+           'cb_subscription_menu_close', 'cb_set_subscription', 'cb_unset_subscription'
+           ]
