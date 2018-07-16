0c23c58384afce9a70182f5d8cd59b35f295246a
gurland <stas.gurland@gmail.com>
2018-07-17 02:45:31 +0300

    Add on conflict ignore user creation on /start command

--- commands/default.py
+++ commands/default.py
@@ -1,17 +1,15 @@
-import telebot.types
 from utils import bot
-
 from text_messages import text_messages
+from models import User
+
+import telebot.types
 
 
 def cmd_welcome(message: telebot.types.Message):
+    User.insert(user_id=message.from_user.id, first_name=message.from_user.first_name,
+                role='User').on_conflict_ignore().execute()
     bot.reply_to(message, text_messages['help'])
 
 
 def cmd_links(message: telebot.types.Message):
     bot.reply_to(message, text_messages['help'])
-
-
-def cmd_lib(message: telebot.types.Message):
-    reply_keyboard = None
-    bot.reply_to(message, 'Список каталогов:', reply_markup=reply_keyboard)
