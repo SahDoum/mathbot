e6caa0d848a614bcac8831cbfc1684d1f10477b6
gurland <stas.gurland@gmail.com>
2018-07-27 02:35:19 +0300

    Add flask and webhook support

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 import os
 import logging
 
+DEBUG = os.getenv('DEBUG', True)
+
 API_TOKEN = os.getenv('SOSISKA_TOKEN', '')
 BOT_NAME = os.getenv('BOT_NAME', '')
 
@@ -8,3 +10,9 @@ LOGGING_LEVEL = int(os.getenv('LOGGING_LEVEL', logging.INFO))
 CHANNEL_NAME = os.getenv('CHANNEL_NAME', '')
 
 IUMURL = "http://ium.mccme.ru/"
+
+# Webhook settings
+PUB_CERT = os.getenv('PUB_CERT', '')
+WEBHOOK_HOST = os.getenv('HOST', '')
+WEBHOOK_URL_PATH = f'/{API_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_URL_PATH}'
